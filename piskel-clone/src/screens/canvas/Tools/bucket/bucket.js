f304b4715bab29a92c390001c1573fb5b08bd5f3
SADmammoth <lo.ma200018@gmail.com>
2019-06-29 20:01:28 +0300

    fix: improvments of bucket tool performance

--- piskel-clone/src/screens/canvas/Tools/bucket/bucket.js
+++ piskel-clone/src/screens/canvas/Tools/bucket/bucket.js
@@ -8,54 +8,45 @@ export default class Bucket extends Tool {
 
 
   async clickAction(event) {
+    let trampoline = fn => (...args) => {
+      let result = fn(...args)
+      while (typeof result === 'function') {
+        result = result()
+      }
+      return result
+    };
     this.start = this.manager.getPointedUnit(event.pageX, event.pageY);
     this.color = this.start.getColor();
     function paint(x) {
       x.paint(null, window.globalState.unit_size, window.globalState.secondaryColor);
     }
-
-    function rec(plus, cross, color, i) {
-      if (!plus.length && !cross.length) {
+    paint(this.start);
+    function rec(plus, color, i) {
+      if (!plus.length) {
+        console.log(`Bucket suspended; array max size ${plus.length}`)
         return;
       }
 
       if (plus.length > 2000) {
         throw Error(`bad: ${plus.length}`);
       }
-      plus = plus.filter((x) => {
-        if (x !== 'no_unit' && x.getColor() === color) {
-          paint(x);
-          return true;
-        }
-        return false;
-      });
 
-      cross = cross.reduce((acc, x) => { acc = addThatNotIn(acc, x.get_cross(1)); return acc; }, []);
-      cross = cross.filter((x) => {
-        if (x !== 'no_unit' && x.getColor() === color) {
-          paint(x);
-          return true;
-        }
-        return false;
-      });
-
-      plus = plus.reduce((acc, x) => { acc = addThatNotIn(acc, x.get_plus(1)); return acc; }, []);
+      plus = plus.reduce((acc, x) => { acc = addThatNotIn(acc, x.get_plus(1), color); return acc; }, []);
 
       i++;
-      setTimeout(rec, 0, plus, cross, color, i);
+      setTimeout(rec, 0, plus, color, i);
     }
 
-    function addThatNotIn(arr1, arr2) {
-      arr2.forEach((x) => { if (inthe(arr1, x) === -1) { arr1.unshift(x); } });
+    function addThatNotIn(arr1, arr2, color) {
+      arr2.forEach((x, i) => { if (inthe(arr1, x, i) === -1 && x.getColor() === color) { paint(x); arr1.push(x); } });
       return arr1;
     }
 
-    function inthe(arr, a) {
+    function inthe(arr, a, m) {
       if (a === 'no_unit') {
         return -2;
       }
-      for (let i = 0; i < arr.length; i++) {
-
+      for (let i = m; i < arr.length; i++) {
         if (arr[i].d_x === a.d_x && arr[i].d_y === a.d_y) {
           return i;
         }
@@ -63,7 +54,7 @@ export default class Bucket extends Tool {
       return -1;
     }
 
-    rec(Array.of(this.start), Array.of(this.start), this.color, 0);
+    trampoline(rec)(Array.of(this.start), this.color, 0);
   }
 
 
@@ -79,52 +70,3 @@ export default class Bucket extends Tool {
 
   deactivateTool() { return true; }
 }
-
-
-
-
-
-
-// this.start = this.manager.getPointedUnit(event.pageX, event.pageY);
-//     this.color = this.start.getColor();
-//     async function paint(arr, color) {
-
-//       for (let i = 0; i < arr.length; i++) {
-
-//         if (arr[i] === null) {
-//           return [];
-//         }
-
-//         if (arr[i].getColor() === color) {
-//           arr[i].paint(null, window.globalState.unit_size, window.globalState.secondaryColor);
-//         } else {
-//           arr.splice(i, 1);
-//         }
-
-//       }
-//       if (arr !== []) {
-//         arr = arr.map((x) => { if (x.dir) { console.log(x[x.dir]); return x[x.dir](); } });
-//       }
-//       return arr;
-//     }
-
-//     async function rec(color, unit, arr, i) {
-
-//       if (i === 1) {
-//         unit.paint(null, window.globalState.unit_size, window.globalState.secondaryColor);
-//         arr = unit.get_neighbors(1);
-//       } else {
-//         arr = await paint(arr, color);
-//       }
-//       if (arr === []) {
-//         return;
-//       }
-
-//       i++;
-//       if (i > 200) {
-//         return;
-//       }
-//       return rec(color, unit, arr, i);
-//     }
-
-//     rec(this.color, this.start, Array.of(this.start), 1);
